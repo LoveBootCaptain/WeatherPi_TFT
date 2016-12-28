a3efea131fbe82ad98e0bb41825dab52b7e76338
lovebootcaptain <loveboatcaptain77@googlemail.com>
2016-12-28 12:59:14 +0100

    updated licence

--- fonts/__init__.py
+++ fonts/__init__.py
@@ -4,6 +4,7 @@
 # MIT License
 #
 # Copyright (c) 2016 LoveBootCaptain (https://github.com/LoveBootCaptain)
+# Author: Stephan Ansorge aka LoveBootCaptain
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
